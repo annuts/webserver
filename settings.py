37cdd6291fa7070d35859925650fed6f0c1b96c4
dewei <zhangdewei@breadtrip.com>
2015-06-19 16:51:09 +0800

    settings与runserver的区分开

--- settings.py
+++ settings.py
@@ -1,24 +1,8 @@
-#!/usr/bin/env python
-#coding:utf-8
-
-import tornado.autoreload
-import tornado.httpserver
-import tornado.options
-import tornado.ioloop
+ #!/usr/bin/env python
+ #conding:utf-8
 
 from urls import app
 
+port = 8080
 
-
-
-app.listen(8080)
-
-tornado.options.parse_command_line()
-http_server = tornado.httpserver.HTTPServer(app)
-instance = tornado.ioloop.IOLoop.instance()
-tornado.autoreload.start(instance)
-
-try:
-    instance.start()
-except:
-    instance.stop()
+app.listen(port)
