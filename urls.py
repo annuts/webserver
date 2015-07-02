3d595528c57e7e652b611de990e79290e81f0979
ZhangDewei <conan868242@sina.com>
2015-07-02 11:28:52 +0800

    添加tornado-rest的一个测试 需要先安装pyrestful

--- urls.py
+++ urls.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 #conding:utf-8
 
+import pyrestful.rest
 import tornado.web
-from main import MainHandler
+from main import *
 from config import settings
 
 
+"""
 app = tornado.web.Application(
     handlers = [
         ('/', MainHandler),
     ],
     **settings
 )
+"""
 
+rest_views = [EchoService,]
+
+app = pyrestful.rest.RestService(rest_views)
