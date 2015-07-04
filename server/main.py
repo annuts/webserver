a6576f667adc958f3c6381c5c5cd111abb6e8836
dewei <zhangdewei@breadtrip.com>
2015-07-04 11:40:35 +0800

    去掉 tornado-rest

--- server/main.py
+++ server/main.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python
 import tornado.web
-import pyrestful.rest
 
-from pyrestful import mediatypes
-from pyrestful.rest import get
 
-"""
 class MainHandler(tornado.web.RequestHandler):
 
  def get(self):
         self.write("Hello, world")
-"""
-
-class EchoService(pyrestful.rest.RestHandler):
-    @get(_path="/api/{name}", _produces=mediatypes.APPLICATION_JSON)
-    def sayHello(self, name):
-	print name
-	return {"Hello": name}
--- server/urls.py
+++ server/urls.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python
 #conding:utf-8
 
-import pyrestful.rest
 import tornado.web
 from main import *
 from config import settings
 
 
-"""
 app = tornado.web.Application(
     handlers = [
         ('/', MainHandler),
     ],
     **settings
 )
-"""
-
-rest_views = [EchoService,]
-
-app = pyrestful.rest.RestService(rest_views)
